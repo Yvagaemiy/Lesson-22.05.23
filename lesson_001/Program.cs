c62e546d4bf05e7dd46154e796fa0af8a5366849
Yvagaemiy <krylatiy2@mail.ru>
2023-05-22 11:56:46 +0300

    ls_001

---
+++ lesson_001/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+
+Console.WriteLine("ведите число");
+//int num = int.Parse(Console.ReadLine()!);
+int num = Convert.ToInt32(Console.ReadLine());
+int resulte = num * num;
+
+Console.WriteLine(resulte);
