27382897d7f75cbe40ad20cf75e220bf7508aef7
Yvagaemiy <krylatiy2@mail.ru>
2023-05-24 18:11:33 +0300

    les_004

---
+++ 002_Lesson/lesson_004/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// 4. Напишите программу, которая принимает на вход 
+// число и проверяет, кратно ли оно одновременно 7 и 
+// 23.
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+//int num = new Random().Next(10, 1000-1);
+Console.WriteLine("Введите чмсло");
+//Console.WriteLine($"сгенерированное число: {num}");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 7 ==0 && num % 23 == 0)
+{
+    Console.WriteLine("Да ");
+} 
+else
+{
+    Console.WriteLine("Нет");
+}
