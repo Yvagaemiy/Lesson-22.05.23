690c2a3babaf2573128e9130728224be40784cfd
Yvagaemiy <krylatiy2@mail.ru>
2023-05-24 10:53:53 +0300

    ls_003

---
+++ lesson_003/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Console.WriteLine("Введитечисло : ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+switch (num)
+{
+    case 1:
+    Console.WriteLine("ПН");
+    break;
+    case 2:
+    Console.WriteLine("ВТ");
+    break;
+    case 3:
+    Console.WriteLine("СР");
+    break;
+    case 4:
+    Console.WriteLine("ЧТ");
+    break;
+    case 5:
+    Console.WriteLine("ПТ");
+    break;
+    case 6:
+    Console.WriteLine("СБ");
+    break;
+    case 7:
+    Console.WriteLine("ВС");
+    break;
+    default:
+     Console.WriteLine("такого дня недели нет");
+    break;
+}
+*/
+Console.WriteLine("Введитечисло : ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+string[] arry = {"пн","вт","ср","чт","пт","сб","вс"};
+
+Console.WriteLine(arry[num-1]);
