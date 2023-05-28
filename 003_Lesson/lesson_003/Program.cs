a447464024887a0bcaa471c26d5e0b00deafb0e0
Yvagaemiy <krylatiy2@mail.ru>
2023-05-28 18:41:20 +0300

    ls_004 fynctoin1

---
+++ 003_Lesson/lesson_003/Program.cs
@@ -0,0 +1,26 @@
+﻿
+// Задача 3: Напишите программу, которая 
+// принимает на вход число (N) и выдаёт таблицу 
+// квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+double NewChislo(double num)
+{
+    double i =1;
+    Console.Write($"{num} -> ");
+
+   while( num <+i)
+   {
+      Console.Write($"{Math.Pow(i, 2)}, ");
+
+        i++;
+   }
+
+}
+ 
+Console.WriteLine(" введите число");
+
+double num = Convert.ToInt32(Console.ReadLine());
+
+NewChislo(num);
---
+++ 003_Lesson/lesson_004/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 2: Напишите программу, которая 
+// принимает на вход координаты двух точек и 
+// находит расстояние между ними в 2D 
+// пространстве.
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+double NewVic(int x, int y,int x1,int y1)
+{
+    double resulte = Math.Sqrt((y1-y)*(y1-y)+(x1-x)*(x1-x));
+    return resulte;
+}
+
+
+Console.Write("x= : ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("y= : ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("x1= : ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("y1= : ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine(NewVic( x, y,x1, y1));
+
+
+    
+
+
+
