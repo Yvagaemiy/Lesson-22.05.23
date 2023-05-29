86bb0442502d7a9cff0d176e23ed27fc1f0b62d7
Yvagaemiy <krylatiy2@mail.ru>
2023-05-29 17:29:38 +0300

    hw_002 tabl cub

---
+++ 003_Lesson/HW3/hw_002/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на
+//  вход число (N) и выдаёт таблицу кубов
+//   чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void Squares(int num)
+{
+    int counte = 1;
+     Console.Write($"{num} -> ");
+
+    while (num >= counte)
+    {
+        Console.Write($"{Math.Pow(counte, 3)}, ");//меняя цифру меняем квадрат числа, куб и тд 
+        counte++;
+    }
+    Console.WriteLine();
+}
+Console.Write("введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Squares(num);
