cabe4345ddb84e08d834791be37f49c94df1de11
Yvagaemiy <krylatiy2@mail.ru>
2023-06-03 18:44:44 +0300

    hw_004 polidro_specm

---
+++ 003_Lesson/HW3/hw_004/Program.cs
@@ -0,0 +1,33 @@
+﻿//   Задача 19 - HARD необязательная
+
+// Напишите программу, которая принимает на вход целое число любой разрядности
+//  и проверяет, является ли оно палиндромом. Через строку нельзя решать само собой.
+
+ int NumberRev (int num)
+{
+  int reminder = 0;
+  int reverse =  0;
+     while (num > 0)
+     {
+          reminder = num % 10;//Получил остаток, разделив число на 10 
+          reverse = (reverse * 10) + reminder;//умножил сумму на 10, а затем добавил напоминание
+          num = num / 10;//Получить частное, разделив число на 10
+     }
+     return reverse;
+     return reminder;
+}
+Console.Write("Ввидите число: ");
+int num = Convert.ToInt32(Console.ReadLine());   
+Console.WriteLine($"Обратный порядок равен: {NumberRev (num)}"); 
+
+if (num == NumberRev (num))
+{
+   Console.WriteLine("Это палиндром!");
+ }
+ else
+ {
+   Console.WriteLine("Это не палиндром!");
+ }
+
+
+
