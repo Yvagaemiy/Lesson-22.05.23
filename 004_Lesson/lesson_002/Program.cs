1c64c5924641e4b79741306078f3b0c6c8ff1e35
Yvagaemiy <krylatiy2@mail.ru>
2023-06-06 14:43:59 +0300

    ls_002 sum_number_void

---
+++ 004_Lesson/lesson_002/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Напишите программу, которая принимает 
+// на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+//друго метод void
+
+void Summa(int num)
+{
+    int resulte=0;
+  for(int i = 1;i<=num;i++)
+   //resulte+=i;//результат плюс старый и счетчик плюс один
+  resulte = resulte+i;//складывает все числа до введенного числа соответственно
+  Console.WriteLine($"сумма чисел от 1 до  {(num)} равно {resulte}");
+  
+}
+
+Console.WriteLine("Ввидите число");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+Summa(num);
