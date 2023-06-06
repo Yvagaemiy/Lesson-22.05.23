aed92b973e1c59e203acc14ca865ea1c1a68e61f
Yvagaemiy <krylatiy2@mail.ru>
2023-06-06 16:22:42 +0300

    ls_004 count_nu_untinumberm

---
+++ 004_Lesson/lesson_004/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 1: Напишите программу, которая принимает 
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+//
+
+int SumNUm(int num)
+{
+    
+    if(num ==0)  return 1;
+    
+   int resulte=0;
+    while (num != 0)  //НЕ РАВЕН нулю
+    {
+    num=num/10;
+    resulte++;
+    }
+    return resulte;
+}
+
+Console.WriteLine("введите число");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"количество цифр в чесле: {SumNUm(num)}");
