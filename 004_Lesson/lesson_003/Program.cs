a45a9c57197cfa95cb5b3042cf4c75c475217cf4
Yvagaemiy <krylatiy2@mail.ru>
2023-06-06 16:10:09 +0300

    ls_003 count_num

---
+++ 004_Lesson/lesson_003/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 1: Напишите программу, которая принимает 
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int SumNUm(int num)
+{
+  int resulte=0;
+  
+while (num > 0)  
+  {
+   num=num/10;
+
+   resulte++;
+  }
+ return resulte;
+}
+
+Console.WriteLine("введите число");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"количество цифр в чесле: {SumNUm(num)}");
