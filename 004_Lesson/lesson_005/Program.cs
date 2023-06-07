38cc24b0bc7b5e58629c3dc8bd7f8653271a75e6
Yvagaemiy <krylatiy2@mail.ru>
2023-06-07 11:21:59 +0300

    ls_005 product_number

---
+++ 004_Lesson/lesson_005/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая 
+// принимает на вход число N и выдаёт 
+// произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int ProductNum(int num)
+{
+    int count=0;
+   if(num==1) 
+   {
+   return 1;
+   }
+   else 
+   {
+  count= num*ProductNum(num-1);
+   }
+   return count;
+}
+
+Console.WriteLine("ВВидите число");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"произведение числа {num}! -> {ProductNum( num)}");
