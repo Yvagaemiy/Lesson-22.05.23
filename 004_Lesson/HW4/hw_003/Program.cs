0523758ddd3797d13981a8b28d8d509b2201205a
Yvagaemiy <krylatiy2@mail.ru>
2023-06-09 15:25:47 +0300

    hw4_003 mass

---
+++ 004_Lesson/HW4/hw_003/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 29: Напишите программу,
+//  которая задаёт массив из 8 элементов
+//   и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+int[] MekArray()
+{
+   int[]arr= new int[8];
+   for(int i = 0;i < arr.Length;i++)
+   {
+    arr[i] = new Random().Next(10);
+    System.Console.Write($"{arr[i]} ");
+   }
+   return arr;
+}
+System.Console.WriteLine(MekArray());
