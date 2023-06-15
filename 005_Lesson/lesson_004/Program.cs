1946eccded84880185023abab41747894974c11b
Yvagaemiy <krylatiy2@mail.ru>
2023-06-15 12:18:16 +0300

    ls_5_004 massiv_

---
+++ 005_Lesson/lesson_004/Program.cs
@@ -0,0 +1,62 @@
+﻿
+// Задача 2: Задайте массив. Напишите программу, которая 
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int[] MekArray(int size, int from, int to)
+{
+   int[] array = new int[size];
+   for (int i = 0; i < size; i++)
+   {
+    array[i] = new Random().Next(from, to + 1);
+   } 
+    return array;
+}
+
+int PrintArray(int[]array)
+{
+  int size = array.Length;
+for (int i = 0; i < size; i++)
+{
+    System.Console.Write($"{array[i]} ");    
+}
+    return size;
+}
+
+int UserNum(int[]array)
+{
+    int num = numuser;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(num==array[i])
+        return "Yes!";
+        
+    }
+    return "No!";
+}
+   
+
+System.Console.Write("Введите длину массива: ");
+int numsize = Convert.ToInt32(Console.ReadLine());
+
+
+System.Console.Write("Введите начало массива: ");
+int start = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите конец массива: ");
+int stop = Convert.ToInt32(Console.ReadLine());
+
+
+int[] mass = MekArray(numsize, start, stop);
+
+System.Console.WriteLine(mass);
+
+PrintArray(mass);
+System.Console.WriteLine();
+
+Console.Write("Введите число: ");
+int numuser = Convert.ToInt32(Console.ReadLine());
+
+ System.Console.WriteLine(UserNum(mass));
+
---
+++ 005_Lesson/lesson_005/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 3: Задайте одномерный массив из 123 случайных чисел. 
+// Найдите количество элементов массива, значения которых лежат в 
+// отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 
+// 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[]MekArray()
+{
+   int[]array = new int[15];
+   for (int i = 0; i < array.Length; i++)
+   {
+    array[i] = new Random().Next(-100, 100 + 1);
+   }
+    return array;
+}
+
+int PrintArray(int[] array)
+{
+ int size = array.Length;
+ for (int i = 0; i < size; i++)
+ {
+    System.Console.Write($"{array[i]} ");
+ }
+    return size;
+}
+
+int Distans (int[]array)
+{
+  int count = 0 ;
+  for (int i = 0; i < array.Length; i++)
+  {
+    
+     if (array[i]  >= 9 && array[i] <= 100)
+     count++;
+  }
+  return count;
+}
+
+int[] mass = MekArray();
+
+PrintArray(mass);
+
+System.Console.WriteLine();
+System.Console.WriteLine($"количество цыфр в отрезке = {Distans (mass)}");
