1a2f7e1a4e2cd9ddead1e40300c7141c0e0d7f9b
Yvagaemiy <krylatiy2@mail.ru>
2023-06-19 11:05:31 +0300

    ls_6_004 dvoichnay

---
+++ 006_Lesson/lesson_004/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу, которая будет преобразовывать 
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+string NewNumber (int num)
+{
+    string numnew = "";
+    while(num > 0)
+  {
+    numnew = Convert.ToString(num % 2) + numnew;
+    num= num/2;
+  
+  }
+   return numnew;
+
+}
+
+System.Console.Write("Введите десятичное число: ");
+
+
+int num =  int.Parse(Console.ReadLine()!);
+
+System.Console.Write($"Двоичное число =          {NewNumber (num)}");
