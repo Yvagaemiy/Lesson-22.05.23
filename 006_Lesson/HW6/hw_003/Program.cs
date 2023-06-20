e63b54b0aff216dcdda6f973a179bc4af509b0a6
Yvagaemiy <krylatiy2@mail.ru>
2023-06-20 12:35:32 +0300

    ls_6_006 copi_array

---
+++ 006_Lesson/HW6/hw_003/Program.cs
@@ -0,0 +1,70 @@
+﻿
+// Задача НЕГАФИБОНАЧЧИ необязательная. Задайте 
+// число. Составьте массив чисел НегаФибоначчи, в 
+// том числе для отрицательных индексов.
+// Пример:
+// для k = 9 массив будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+
+void Fibonach(int []arr)
+{
+    arr[arr.Length/2] = 0;
+    arr[arr.Length/2+1] = 1;
+    arr[arr.Length/2-1] = 1;
+    // arr[0]=0;
+    // arr[1]=1;
+    // arr[2]=1;
+
+    for (int i =arr.Length/2+2; i < arr.Length; i++)
+    {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+ 
+    for (int i = 0; i < arr.Length /2-1 ; i++)
+   {
+    arr[i] = arr[arr.Length-1-i];
+    if(i % 2 == 0) 
+    arr[i]= arr[i] * -1;
+//     // temp = array[array.Length - 1 - i]*(-1);
+//     // array[array.Length - 1 - i] = array[i];
+//     // array[i] = temp;
+   
+//     //array[i] = temp*(-1);
+
+   }
+
+
+}
+
+void PrintArray(int[]array)
+{
+    for (int i = 0;i < array.Length; i++)
+    {
+        // System.Console.Write($"{array[i]*(-1)}, {array[i]}");
+        System.Console.Write($"{array[i]} ");
+    }
+}
+
+// void Rev(double[] ar)
+// {
+// for (int i = 0; i < ar.Length; i++)
+
+//     while (ar[i] != 0);
+
+//    ar[ar.Length] --;
+
+// }
+System.Console.Write("Введите число");
+int num = int.Parse(Console.ReadLine()!);
+
+int[]mass = new int [ num*2-1];
+
+Fibonach(mass);
+PrintArray(mass);
+
+// RevesArray(mass);
+// PrintArray(mass);
+
+
+
+
---
+++ 006_Lesson/lesson_006/Program.cs
@@ -0,0 +1,53 @@
+﻿
+// Задача 4: Напишите программу, которая будет создавать 
+// копию заданного массива с помощью поэлементного 
+// копирования.
+
+double[] MekArray(int size, int from, int to)
+{
+   double[] array = new double[size];
+   for (int i = 0; i < size; i++)
+   {
+     array[i] = new Random().Next(from, to+1);
+   }
+    return array;
+}
+
+int PrintArray(double []array)
+{
+   int size = array.Length;
+   for (int i = 0; i < size; i++)
+   {
+     System.Console.Write($"{array[i]} ");
+   }
+    return size;
+}
+double[] CopArray(double[]arr)
+{
+    
+    int size = arr.Length;
+   double[] new_arr = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+       new_arr[i]= arr[i]  ;
+    }
+    return new_arr;
+}
+
+
+System.Console.Write("Введите длину массива: ");
+int numsize = int.Parse(Console.ReadLine()!);
+
+System.Console.Write("Введите начало массива: ");
+int start = int.Parse(Console.ReadLine()!);
+
+System.Console.Write("Введите конец массива: ");
+int stop = int.Parse(Console.ReadLine()!);
+
+double[]mass = MekArray(numsize,start,stop);
+PrintArray(mass);
+
+System.Console.WriteLine();
+
+double[]mass_1 = CopArray(mass);
+PrintArray(mass_1);
