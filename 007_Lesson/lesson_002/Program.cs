b59b6f7a1f2e59d5a814bad573e66602d992ca0a
Yvagaemiy <krylatiy2@mail.ru>
2023-06-21 16:23:50 +0300

    ls_7_002 [i,j]*[i,j]

--- 007_Lesson/lesson_002/Program.cs
+++ 007_Lesson/lesson_002/Program.cs
@@ -13,56 +13,60 @@
 // 5 81 2 9
 // 8 4 2 4
 
+int[,] FullMatrix(int row, int  column, int from, int to )
+{
+   int[,]matrix = new int[row, column];
 
-int[,] FullMatrix(int[,]array )
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    
-        array[i,j] = new Random().Next(-30,30);
-  } 
-  return array;
+   for (int i = 0; i < row; i++)
+   {
+         for (int j = 0; j < column; j++)
+         matrix[i,j] = new Random().Next(from, to + 1);     
+   }  
+  return matrix;
 }
 
-void PrintArray(int[,]array)
+//int PrintMatrix(int[,]mat)
+void PrintMatrix(int[,]mat)
 {
-    
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    
-        System.Console.Write($"{array[i,j],3}");
-       System.Console.WriteLine();
-  }
- 
+ int row_size = mat.GetLength(0);
+ int column_size = mat.GetLength(1);
+    for (int i = 0; i < row_size; i++)
+    {   
+        for (int j = 0; j < column_size; j++) 
+         System.Console.Write($"{mat[i,j],3} ");//резервируем 3 ячейки
+         System.Console.WriteLine();//для перехода на новую строку         
+    }  
+    // return row_size;
+    // return column_size;
 }
-void NewArray(int [,]array)
+void NewArray(int [,]ar)
 {
-    if (array.GetLength(0)%2==0)
-    for (int i = 0; i < array.GetLength(0); i++)
-    
-      array[i]= array[i]*array[i];
-    
-     if (array.GetLength(1)%2==0)
-    for (int j = 0; j < array.GetLength(1); j++)
-    
-      array[j]= array[j]*array[j];
-    
-    // return array[i];
-    // return array[j];
+    int row_size = ar.GetLength(0);
+    int column_size = ar.GetLength(1);
+    for (int i = 1; i < row_size; i+=2)
+        {   
+        for (int j = 1; j < column_size; j+=2)
+        ar[i,j]= ar[i,j]*ar[i,j];
+        }
 }
+
 System.Console.Write("Введите количество строк: ");
 int row_s = Convert.ToInt32(Console.ReadLine());
 
 System.Console.Write("Введите количество столбцов: ");
 int column_s = Convert.ToInt32(Console.ReadLine());
 
+System.Console.Write("Введите начало массива: ");
+int start = Convert.ToInt32(Console.ReadLine());
 
-int[,]new_matrix =  new int[row_s, column_s];
-FullMatrix(new_matrix);
-PrintArray(new_matrix);
+System.Console.Write("Введите конец массива: ");
+int stop = Convert.ToInt32(Console.ReadLine());
 
-int[,]new_matrix_1 = NewArray();
+
+int[,]new_matrix =  FullMatrix( row_s, column_s, start, stop );
+//FullMatrix(new_matrix);
+PrintMatrix(new_matrix);
+
+System.Console.WriteLine();
 NewArray(new_matrix);
-PrintArray(new_matrix_1);
+PrintMatrix(new_matrix);
