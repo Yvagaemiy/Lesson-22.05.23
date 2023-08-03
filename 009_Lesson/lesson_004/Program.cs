7abf6c047d708820a6ad0f107150bbcf4f94dbb6
Yvagaemiy <krylatiy2@mail.ru>
2023-08-03 14:06:17 +0300

    ls_9_004

---
+++ 009_Lesson/lesson_004/Program.cs
@@ -0,0 +1,28 @@
+﻿
+// Задача 3: Напишите программу, которая будет принимать на вход число и 
+// возвращать сумму его цифр. Использовать рекурсию.
+// 453 -> 12
+// 45 -> 9
+
+int NewNum(int num)
+{
+  int count = 0;
+  while(num>0)
+  {
+  count = count + num % 10;
+  num = num/10; 
+  }
+  return count;
+}
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"сумма цифр в числе = {NewNum(num)}");
+
+int NewRec(int num)
+{
+    if(num ==0) return 0  ;
+   return num %10 + NewRec(num/10);
+
+}
+System.Console.WriteLine($"рекурсия сумма цифр в числе = {NewRec( num)}");
