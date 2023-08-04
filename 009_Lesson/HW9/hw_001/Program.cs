ca1393c9690828e31b7c7bf1bac1e0245b3d370d
Yvagaemiy <krylatiy2@mail.ru>
2023-08-04 17:08:52 +0300

    ls_9_006

---
+++ 009_Lesson/HW9/hw_001/Program.cs
@@ -0,0 +1,39 @@
+﻿//  Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1 при помощи рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 6 -> "6, 5, 4, 3, 2, 1"
+int NUmber(int num)
+{
+   int number = 1;
+   while (number < num)
+   {
+    System.Console.Write($"{number}, ");
+    number++;
+   }
+  
+
+    return number;
+}
+
+void NumRec(int num, int count)
+{
+    int count = 1;
+    if (count< num ) return ;
+    
+    NumRec( num,count + 1);
+   System.Console.Write (NumRec(num,count));  
+
+}
+
+
+System.Console.Write("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(NUmber(num));
+
+System.Console.WriteLine($"рекурсия: ");
+NumRec( num, count);
+
+
+
+
---
+++ 009_Lesson/lesson_006/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все 
+// чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+
+
+
+void ValuesSet(int start, int finish)
+{
+    if (start == finish) return ;
+    ValuesSet(start,finish-1 );
+    if (finish % 2==0 )
+    {
+        Console.Write($"{(finish )}, ");
+    }
+}
+System.Console.Write("введите число M: ");
+int start = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("введите число N: ");
+int finish = Convert.ToInt32(Console.ReadLine());
+ValuesSet(start,  finish);
