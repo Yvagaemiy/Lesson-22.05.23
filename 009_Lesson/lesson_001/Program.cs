c59db4c8b2d5a8986af0a0e3cfa2096b38d67730
Yvagaemiy <krylatiy2@mail.ru>
2023-08-04 20:34:38 +0300

    ls_9_007/2

--- 009_Lesson/lesson_001/Program.cs
+++ 009_Lesson/lesson_001/Program.cs
@@ -1,26 +1,28 @@
 ﻿// Посчитать сумму всех целых чисел от 1 до N,
 //  N вводится пользователем
 
-int Sum(int num)
-{
-   int sum = 0;
-   while(true)   //безконечный цикл
-  {
-      sum = sum+num;
-      sum--;
-      if (num==0)
-      break;
-  }
-   return sum;
-}
-int SumRec(int num)
-{
-    if (num==0) return 0;  // это выключатель рекурсии
-    return num + SumRec(num - 1);
+// int Sum(int num)
+// {
+//    int sum = 0;
+//    while(true)   //безконечный цикл
+//   {
+//       sum = sum+num;
+//       sum--;
+//       if (num==0)
+//       break;
+//   }
+//    return sum;
+// }
+// int SumRec(int num)
+// {
+//     if (num==0) return 0;  // это выключатель рекурсии
+//     return num + SumRec(num - 1);
 
-}
+// }
 
-System.Console.Write("введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine($"сумма всех целых чисел от 1 до {num} ровна {Sum(num)}");
-System.Console.WriteLine($"решение рекурсией = {SumRec(num)}");
+// System.Console.Write("введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"сумма всех целых чисел от 1 до {num} ровна {Sum(num)}");
+// System.Console.WriteLine($"решение рекурсией = {SumRec(num)}");
+
+
--- 009_Lesson/lesson_007/Program.cs
+++ 009_Lesson/lesson_007/Program.cs
@@ -4,25 +4,46 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8 -> 30
 
-    int Sum (int s, int f)
-    {
-       int count = s;
-       if (s==f) return 0 ;
-       s++;
-       count= s + Sum ( s,  f);
-       return count;
+    // int Sum (int s, int f)
+    // {
+    //    int count = s;
+    //    if (s==f) return 0 ;
+    //    s++;
+    //    count= s + Sum ( s,  f);
+    //    return count;
 
-     }
-    void SumSum(int s, int f)
-    {
-     System.Console.Write($"сумма чисел от {s} до {f} = {Sum(s-1, f)}");
-    }
+    //  }
+    // void SumSum(int s, int f)
+    // {
+    //  System.Console.Write($"сумма чисел от {s} до {f} = {Sum(s-1, f)}");
+    // }
 
-    System.Console.Write("введите число M: ");
-    int s = Convert.ToInt32(Console.ReadLine());
+    // System.Console.Write("введите число M: ");
+    // int s = Convert.ToInt32(Console.ReadLine());
 
-    System.Console.Write("введите число N: ");
-    int f = Convert.ToInt32(Console.ReadLine());
+    // System.Console.Write("введите число N: ");
+    // int f = Convert.ToInt32(Console.ReadLine());
 
-    System.Console.WriteLine($"не понятное число {Sum (s,f)}");
-    SumSum(s, f);
+    // System.Console.WriteLine($"не понятное число {Sum (s,f)}");
+    // SumSum(s, f);
+
+
+
+// 2 вариант не понимаю почему так происходит
+    int SumRec(int s, int f)
+{
+    if (s>=f) return 0;  // это выключатель рекурсии
+    return  f+ SumRec(s, f-1);
+}
+void Rec(int s, int f)
+{
+    System.Console.Write($"рекурсия правельный ответ {SumRec( s-1, f)}");
+}
+
+System.Console.Write("введите число: ");
+int s = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("введите число: ");
+int f = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"решение рекурсией  ответ не правельный = {SumRec(s,f)}");
+Rec( s, f);
