f2d58fe1d0a25e11cb9bd0686646b50b7261e75e
Yvagaemiy <krylatiy2@mail.ru>
2023-08-04 17:37:34 +0300

    ls_9_006/2

--- 009_Lesson/lesson_006/Program.cs
+++ 009_Lesson/lesson_006/Program.cs
@@ -11,13 +11,28 @@ void ValuesSet(int start, int finish)
     if (start == finish) return ;
     ValuesSet(start,finish-1 );
     if (finish % 2==0 )
-    {
-        Console.Write($"{(finish )}, ");
-    }
+   
+     Console.Write($"{(finish )}, ");
+    
 }
 System.Console.Write("введите число M: ");
 int start = Convert.ToInt32(Console.ReadLine());
 
 System.Console.Write("введите число N: ");
 int finish = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("рекурсия: ");
 ValuesSet(start,  finish);
+
+System.Console.WriteLine();
+System.Console.Write("функция : ");
+
+void NewNum(int start, int finish)
+{
+for (int i = start ;i <= finish; i++)
+    {
+      if( i % 2 ==0)
+      Console.Write($"{i}, ");
+    }
+}
+NewNum( start, finish);
---
+++ 009_Lesson/lesson_007/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+Console.Write("Введите  число : ");
+
+int start = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите  число : ");
+
+int finish = Convert.ToInt32(Console.ReadLine());
+
+
+for (int i = start ;i <= finish; i++)
+
+{
+     if( i % 2 ==0)
+    Console.Write(i + ",");
+}
